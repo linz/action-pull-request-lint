8a89f792d9df99e017b7dd980d5a90a8a1e3fb04
Blayne Chard <bchard@linz.govt.nz>
2023-10-18 09:45:01 +1300

    feat: apply default prettier

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 
 Composite action to checkout/build/test typescript with npm or yarn.
 
-usage: 
+usage:
 
 ```yaml
 name: Pull Request lint
 
 on:
   pull_request:
-    types: ['opened', 'edited', 'reopened', 'synchronize']
+    types: ["opened", "edited", "reopened", "synchronize"]
 
 jobs:
   pull-request-lint:
     runs-on: ubuntu-latest
     steps:
-    - uses: linz/action-pull-request-lint@v0
-      with:
-        scopes: 'infra,sprites,server' # optional list of conventional commit scopes
-        jira-projects: 'BM,TDE' # optional list of jira projects
+      - uses: linz/action-pull-request-lint@v0
+        with:
+          scopes: "infra,sprites,server" # optional list of conventional commit scopes
+          jira-projects: "BM,TDE" # optional list of jira projects
 ```
--- __test__/action.test.mjs
+++ __test__/action.test.mjs
@@ -1,51 +1,64 @@
-import fs from 'node:fs'
-import { describe, it } from 'node:test'
-import assert from 'node:assert'
+import fs from "node:fs";
+import { describe, it } from "node:test";
+import assert from "node:assert";
 
 /**
  * Read the action.yml and create a function from the script inside.
  * replacing {{ inputs.* }} with ctx
- * @param {*} ctx 
+ *
+ * @param {*} ctx
  * @returns Function that requires a `context` and `core` parameter
  */
 function createTestFunction(ctx) {
-  const func = fs.readFileSync('./action.yml', 'utf-8').split('script: |')[1]
+  const func = fs.readFileSync("./action.yml", "utf-8").split("script: |")[1];
 
   const newFunc = func
-    .replace('{{ inputs.scopes }}', ctx.scopes ?? '')
-    .replace('{{ inputs.jira-projects }}', ctx.jiraProjects ?? '')
-    .replace('{{ inputs.jira-required }}', ctx.jiraRequired ?? true)
+    .replace("{{ inputs.scopes }}", ctx.scopes ?? "")
+    .replace("{{ inputs.jira-projects }}", ctx.jiraProjects ?? "")
+    .replace("{{ inputs.jira-required }}", ctx.jiraRequired ?? true);
 
-  return new Function('context', 'core', newFunc)
+  return new Function("context", "core", newFunc);
 }
 
 function runAction(ctx, title) {
-  const action = createTestFunction(ctx)
+  const action = createTestFunction(ctx);
 
   let failed;
-  const core = { setFailed(reason) { failed = reason } }
+  const core = {
+    setFailed(reason) {
+      failed = reason;
+    },
+  };
 
-  action({ payload: { pull_request: { title } } }, core)
+  action({ payload: { pull_request: { title } } }, core);
 
   return failed;
 }
 
+describe("action", () => {
+  it("should validate against jira projects", () => {
+    assert.equal(runAction({}, "Hello BM-14"), undefined);
+    assert.equal(
+      runAction({}, "Hello BM"),
+      "Pull request title does not contain a JIRA ticket!"
+    );
+    assert.equal(runAction({ jiraRequired: false }, "Hello"), undefined);
+  });
 
+  it("should validate against required jira projects", () => {
+    assert.equal(runAction({ jiraProjects: "BM" }, "Hello BM-14"), undefined);
+    assert.equal(
+      runAction({ jiraProjects: "TDE,BM" }, "Hello TDE-1234"),
+      undefined
+    );
 
-describe('action', () => {
-  it('should validate against jira projects', () => {
-    assert.equal(runAction({}, 'Hello BM-14'), undefined)
-    assert.equal(runAction({}, 'Hello BM'), 'Pull request title does not contain a JIRA ticket!')
-    assert.equal(runAction({ jiraRequired: false }, 'Hello'), undefined)
-  })
-
-  it('should validate against required jira projects', () => {
-    assert.equal(runAction({ jiraProjects: 'BM' }, 'Hello BM-14'), undefined)
-    assert.equal(runAction({ jiraProjects: 'TDE,BM' }, 'Hello TDE-1234'), undefined)
-
-    assert.equal(runAction({ jiraProjects: 'TDE,BM' }, 'Hello TE-1234'), 'Pull request title does not contain a JIRA ticket!')
-    assert.equal(runAction({ jiraProjects: 'TDE,BM' }, 'Hello -1234'), 'Pull request title does not contain a JIRA ticket!')
-  })
-})
-
-
+    assert.equal(
+      runAction({ jiraProjects: "TDE,BM" }, "Hello TE-1234"),
+      "Pull request title does not contain a JIRA ticket!"
+    );
+    assert.equal(
+      runAction({ jiraProjects: "TDE,BM" }, "Hello -1234"),
+      "Pull request title does not contain a JIRA ticket!"
+    );
+  });
+});
